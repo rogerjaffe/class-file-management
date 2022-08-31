a9a89ba5dd2731f7e3a781d93af5b0bfcf3e80f9
Roger Jaffe <rogerjaffe@gmail.com>
2022-08-31 13:05:31 -0700

    Update to production student list

--- gcf/constants.js
+++ gcf/constants.js
@@ -1,5 +1,5 @@
 exports.SCOPE = "https://www.googleapis.com/auth/spreadsheets";
-exports.STUDENT_SPREADSHEET_ID = "1kt-sNNra5GOIe6WLQV1_EyJOAkF-GUpP_5mVI7URoMc";
+exports.STUDENT_SPREADSHEET_ID = "1f35OprNqcF6_dHwv3N5rnrWVkGZJyTD8PFaB_TVJF0M";
 exports.STUDENT_SHEET_NAME = "ID-Password";
 exports.PAT_SPREADSHEET_ID = "1P2ZG87q4nwhT2C-qxN1f40m9Caw0C19QzOSMvF-jtNY";
 exports.PAT_SHEET_NAME = "PATs";
